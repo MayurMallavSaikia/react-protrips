a6d97043651feaaa4f758462426012ae009044c4
Mayur Mallav Saikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-03 16:53:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -68,12 +68,12 @@ export class App extends React.Component {
                             <Route path="react-protrips/"  element={<TripsCount total={this.countDays()} trek={this.countDays('Trek')} tropic={this.countDays('Tropic')} club={this.countDays('Club')}/>}  />
                             <Route path="react-protrips/list" element={ <TripsList days={this.state.allTrips} />}/>
 
-                             <Route path="react-protrips/trek" element={<Trek days={this.state.allTrips}/>}  />
-                             <Route path="react-protrips/tropic" element={<Tropic days={this.state.allTrips}/>}  />
-                             <Route path="react-protrips/club" element={<Club days={this.state.allTrips} />} />
+                             <Route path="/react-protrips/trek" element={<Trek days={this.state.allTrips}/>}  />
+                             <Route path="/react-protrips/tropic" element={<Tropic days={this.state.allTrips}/>}  />
+                             <Route path="/react-protrips/club" element={<Club days={this.state.allTrips} />} />
     
                             <Route path="react-protrips/add" element={<AddTrip   newTrip={this.addTrip} />}  />
-                            <Route path="" element={<Error404/>} />
+                            <Route path="*" element={<Error404/>} />
                         </Routes>
                     </div>                    
                 </BrowserRouter>                
